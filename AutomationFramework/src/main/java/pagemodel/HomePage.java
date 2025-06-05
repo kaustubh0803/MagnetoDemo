136ef3d6fa79cac8f78d635141168ddb6f2bc50d
DEX <DEX@LAPTOP-J71GRUG2.bbrouter>
2025-06-05 11:02:40 +0530

    Second Commit - Created structure for pages classes for POM and added POM.xml dependencies for selenium and TESTNG

---
+++ AutomationFramework/src/main/java/pagemodel/HomePage.java
@@ -0,0 +1,16 @@
+package pagemodel;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+
+	public WebDriver driver;
+	public HomePage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	
+}
