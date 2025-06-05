1f4411af4d2865442fe7a9eb9d1d092c56c05d55
DEX <DEX@LAPTOP-J71GRUG2.bbrouter>
2025-06-05 11:43:59 +0530

    Third Commit - Created logic for HomePage and CreateAccountpage in POM

--- AutomationFramework/src/main/java/pagemodel/CreateAccountPage.java
+++ AutomationFramework/src/main/java/pagemodel/CreateAccountPage.java
@@ -1,5 +1,34 @@
 package pagemodel;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class CreateAccountPage {
 
+	public WebDriver driver;
+	
+	public CreateAccountPage(WebDriver driver) {
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy(id="firstname")
+	WebElement firstName;
+	
+	@FindBy(id="lastname")
+	WebElement lastName;
+	
+	@FindBy(id="email_address")
+	WebElement emailAddress;
+	
+	@FindBy(id="password")
+	WebElement password;
+	
+	@FindBy(id="password-confirmation")
+	WebElement confirmedPassword;
+	
+	@FindBy(css=".submit span")
+	WebElement createAccountButton;
 }
--- AutomationFramework/src/main/java/pagemodel/HomePage.java
+++ AutomationFramework/src/main/java/pagemodel/HomePage.java
@@ -2,15 +2,29 @@ package pagemodel;
 
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 
 	public WebDriver driver;
+	
 	public HomePage(WebDriver driver) {
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
 	}
 	
+	@FindBy(xpath="//a[contains(@href,'login')]")
+	WebElement signinButton;
 	
+	@FindBy(xpath="//a[contains(text(),'Create an Account')]")
+	WebElement signupButton;
+	
+	@FindBy(css=".base")
+	WebElement homePageHeader;
+	
+	public String getHomePageHeaderText() {
+		return homePageHeader.getText();
+	}
 }
